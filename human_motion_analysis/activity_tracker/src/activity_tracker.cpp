82b4b631e298bee9f11d712df2dd8da49e7526ab
Tarik Kelestemur <tkelestemur@gmail.com>
2015-06-28 16:50:09 -0400

    activity_tracker minor changes

--- human_motion_analysis/activity_tracker/src/activity_tracker.cpp
+++ human_motion_analysis/activity_tracker/src/activity_tracker.cpp
@@ -21,11 +21,11 @@ int main(int argc, char **argv)
 
     tf::TransformListener listener;
     std::string openni_depth_frame;
-    std::string targetFrames[]={"/left_knee_1","/right_foot_1","/right_knee_1","/right_foot_1" };
-    std::string targetFrames1[]={"/left_knee_1","/right_knee_1"};
-    std::string reference_frame="/torso_1";
-    std::string reference_frame1="/left_hip_1";
-    std::string reference_frame2="/right_hip_1";
+    std::string targetFrames[]={"/left_knee_1","/left_foot_1","/right_knee_1","/right_foot_1" };
+    //std::string targetFrames1[]={"/left_knee_1","/right_knee_1"};
+    //std::string reference_frame="/torso_1";
+    //std::string reference_frame1="/left_hip_1";
+    //std::string reference_frame2="/right_hip_1";
 
     n.getParam("camera_frame_id", openni_depth_frame);
     ROS_INFO("%s", openni_depth_frame.c_str());
@@ -36,10 +36,10 @@ int main(int argc, char **argv)
         std::stringstream ss;
         ss << count;
 
-        for(int i=0; i<4; i++){
+        // for(int i=0; i<4; i++){
             try{
                 tf::StampedTransform transform;
-                listener.lookupTransform(reference_frame, targetFrames[i], ros::Time(0), transform);
+                listener.lookupTransform(targetFrames[1], targetFrames[3], ros::Time(0), transform);
 
                 // ss << " , "<<targetFrames[i]<<" , "<<transform.getOrigin().getX()<<" , "
                 //    <<transform.getOrigin().getY()<<" , "
@@ -49,33 +49,27 @@ int main(int argc, char **argv)
                 //    <<transform.getRotation().getZ()<<" , "
                 //    <<transform.getRotation().getW();
 
-                  ss << " , "<<targetFrames[i]<<" , "
+                  ss << " , "<<"Rotation:"<<" , "
                      << transform.getRotation().getX()<<" , "
                      << transform.getRotation().getY()<<" , "
                      << transform.getRotation().getZ()<<" , "
                      << transform.getRotation().getW();
 
 
-                  ss << " , "<<targetFrames[i]<<" , "
+                  ss << " , "<<"Distance:"<<" , "
                      << transform.getOrigin().getX()<<" , "
                      << transform.getOrigin().getY()<<" , "
-                     << transform.getOrigin().getZ();
+                     << transform.getOrigin().getZ()<<" , ";
 
-
-
-
-
-
-
-            }
-
-
+		  }
+        //
+        //
             catch (tf::TransformException ex){
                 ROS_ERROR("%s",ex.what());
                 ros::Duration(1.0).sleep();
             }
-
-        }
+        //
+        // }
 
         // tf::StampedTransform transform;
         // listener.lookupTransform(reference_frame1, targetFrames1[0], ros::Time(0), transform);
